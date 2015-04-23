527f43f4f7316738be6c0510cb9ec531572fbecd
Kate Miller <kate.miller237@gmail.com>
2015-04-23 17:11:48 -0400

    Initial project upload with tweets file

---
+++ workspace/ivy-tweets/src/TweetParser.java
@@ -0,0 +1,14 @@
+
+public class TweetParser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		read("300k_tweets.txt"); 
+	}
+	
+	
+	private static void read(String filename) {
+		
+	}
+
+}
